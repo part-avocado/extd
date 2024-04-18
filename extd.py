3afa2712ff9e84e39dfe5e78023f346a940f5d51
partavo <153518510+partavo@users.noreply.github.com>
2024-04-18 08:11:01 -0400

    Update extd.py

--- extd.py
+++ extd.py
@@ -1,10 +1,10 @@
 import sys
-from exit_err import exit_err
-from err import err
+from errocodes import err
 
 def errorExit(code, message = None):
-  exit_err(code, message)
+  err(code, message)
   sys.exit()
 def raiseError(code, message = None):
   err(code, message)
-  return
+  return code
+
